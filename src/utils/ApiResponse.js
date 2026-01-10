36cebe1076871000174a9379c8076e5861fcab64
Kushal Sahu <kushalsahu352@gmail.com>
2026-01-10 10:43:02 +0530

    utils code

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResponse{
+    constructor(statuseCode,data,message="success"){
+        this.statuseCode=statuseCode
+        this.data = data
+        this.message = message
+        this.success = statuseCode<400
+    }
+}
