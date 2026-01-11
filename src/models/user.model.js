43c6ae9e92de09170ab28c4d77a5c6ba05799e2c
Kushal Sahu <kushalsahu352@gmail.com>
2026-01-11 21:55:17 +0530

    cludinary setup

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -54,11 +54,11 @@ const userSchema = new Schema(
 
 userSchema.pre("save", async function (next){
     if(!this.isModified("password")) return next();
-    this.password = bcrypt.hash(this.password, 10)
+    this.password =await bcrypt.hash(this.password, 10)
     next()
 } )
 
-userSchema.methods.isPawwordCorrect = async function(password){
+userSchema.methods.isPasswordCorrect = async function(password){
    return await bcrypt.compare(password, this.password)
 }
 
@@ -68,7 +68,7 @@ userSchema.method.generateAccessToken = function(){
             _id:this._id,
             email:this.email,
             username:this.username,
-            fullName:this.fullname
+            fullName:this.fullName
         },
         process.env.ACCESS_TOKEN_SECRET,
         {
@@ -76,7 +76,7 @@ userSchema.method.generateAccessToken = function(){
         }
     )
 }
-userSchema.method.generateRefreshToken = function(){
+userSchema.methods.generateRefreshToken = function(){
      return jwt.sign(
         {
             _id:this._id,
